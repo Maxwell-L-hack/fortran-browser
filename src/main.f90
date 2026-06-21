855c6477eecb323091aaf309b996ad35e323e531
Maxwell Lenhoff <maxwelll@hackclub.app>
2026-06-21 17:15:33 -0500

    Attempted to get the window opening when gui is entered into the terminal

--- src/main.f90
+++ src/main.f90
@@ -1,14 +1,22 @@
 module handlers
 
   use, intrinsic :: iso_c_binding
-  use gtk
+  use gtk, only: gtk_application_new, G_APPLICATION_FLAGS_NONE, gtk_application_window_new, gtk_widget_show, gtk_window_set_title
+  use g, only: g_application_run, g_object_unref
   implicit none
 
   contains
-    subroutine activate
-    end subroutine activate
-end module handlers
 
+    subroutine activate(app, gdata) bind(c)
+      type(c_ptr), value, intent(in) :: app, gdata
+      type(c_ptr) :: window
+
+      window = gtk_application_window_new(app)
+      call gtk_window_set_title(window, "the Fortran Web Browser")
+      call gtk_widget_show(window)
+    end subroutine activate
+    
+end module handlers
 
 program simple_get
   use, intrinsic :: iso_c_binding
@@ -18,16 +26,19 @@ program simple_get
   use g
 
   implicit none
-  integer(c_int)      :: status
-  type(c_ptr)         :: app
 
-  app = gtk_application_new("Maxwell-L-hack.gtk-fortran-browser")
+  type(c_ptr)         :: app
+  integer(c_int)      :: status
 
   type(response_type) :: response
 
   character(len=2048) :: url_string
   character(len=:), allocatable :: url_input
   character(len=:), allocatable :: user_platform_string
+  character(len=:), allocatable :: body
+
+  app = gtk_application_new("Maxwell-L-hack.fortran-browser"//c_null_char, G_APPLICATION_FLAGS_NONE)
+  status = g_application_run(app, 0_c_int, [c_null_ptr])
 
   print *,'Enter the URL you wish to view:'
   read (*,'(A)') url_string
@@ -41,7 +52,7 @@ program simple_get
     
   response = request(url=url_input)
 
-  print *,'Enter the platform you''re on, either terminal or gui.'
+  print *,'Enter the platform you wish to use, either terminal or gui.'
   read (*,'(A)') user_platform_string
   user_platform_string = trim(adjustl(user_platform_string))
 
@@ -57,9 +68,16 @@ program simple_get
     print *, response%content
   end if
     
+  body = response%content
+
   if (user_platform_string == 'terminal') then
     print *, 'Response Content (tags removed):'
   end if
 
+  if (user_platform_String == 'gui') then
+    call g_signal_connect(app, "activate"//c_null_char, c_funloc(activate), c_null_ptr)
+  end if
+  
+  call g_object_unref(app)
 
 end program simple_get
